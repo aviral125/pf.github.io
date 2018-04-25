43af20545623d0f9d83f207b21975168c1c23d0d
aviral125 <aviral.125@gmail.com>
2018-04-26 05:08:10 +0530

    final push

---
+++ main.js
@@ -0,0 +1,170 @@
+$(document).ready(function(){
+$('.nav-lis').on('click', 'div', function() {
+    $('.nav-lis div.acti').removeClass('acti');
+    $(this).addClass('acti');
+
+});
+$('.nav-lists').on('click', 'div', function() {
+    $('.nav-lists div.acti').removeClass('acti');
+    $(this).addClass('acti');
+
+});
+
+$('#mobi').click(function() {
+
+    	// console.log("roit");
+    	$('.ismobile').css("display","block");
+
+    $('.isdesktop').css("display","none");
+    $('.istv').css("display","none");
+    $('.ismumbai').css("display", "none");
+ $('.ismobile').slick({
+   lazyload:'ondemand',
+    centerMode: false,
+  slidesToShow: 1.6,
+  dots: true,
+  infinite: false,
+  speed: 300,
+  arrows: true,
+            prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+            nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
+
+  });
+
+});
+
+
+$('#deskt').click(function() {
+
+    	$('.isdesktop').css("display","block");
+   		$('.istv').css("display","none");
+   		$('.ismobile').css("display","none");
+      $('.ismumbai').css("display", "none");
+
+$('.isdesktop').slick({
+    centerMode: false,
+  slidesToShow: 1.2,
+  dots: true,
+  infinite: false,
+  speed: 300,
+  arrows: true,
+            prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+            nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
+
+  });
+});
+$('#smart').click(function() {
+
+    	$('.istv').css("display","block");
+   	$('.isdesktop').css("display","none");
+   		$('.ismobile').css("display","none");
+      $('.ismumbai').css("display", "none");
+ $('.istv').slick({
+    centerMode: false,
+  slidesToShow: 1,
+  dots: true,
+  infinite: false,
+  speed: 300,
+  arrows: true,
+            prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+            nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
+
+  });
+
+
+});
+$('#exp').click(function() {
+
+    	// console.log("roit");
+    	$('.isexpert').css("display","block");
+
+    $('.isintermediate').css("display","none");
+    $('.isnew').css("display","none");
+ $('.isexpert').slick({
+   lazyload:'ondemand',
+    centerMode: false,
+  slidesToShow: 1.6,
+  dots: true,
+  infinite: false,
+  speed: 300,
+  arrows: true,
+            prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+            nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
+
+  });
+
+});
+
+
+$('#int').click(function() {
+
+    	$('.isintermediate').css("display","block");
+   		$('.isexpert').css("display","none");
+   		$('.isnew').css("display","none");
+
+$('.isintermediate').slick({
+    centerMode: false,
+  slidesToShow: 1.6,
+  dots: true,
+  infinite: false,
+  speed: 300,
+  arrows: true,
+            prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+            nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
+
+  });
+});
+$('#new').click(function() {
+
+    	$('.isnew').css("display","block");
+   	$('.isexpert').css("display","none");
+   		$('.isintermediate').css("display","none");
+ $('.isnew').slick({
+    centerMode: false,
+  slidesToShow: 1.6,
+  dots: true,
+  infinite: false,
+  speed: 300,
+  arrows: true,
+            prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+            nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
+
+  });
+
+
+});
+
+
+
+$('#mum').click(function() {
+
+    	$('.ismumbai').css("display","block");
+   	$('.isbengaluru').css("display","none");
+   		$('.iskolkata').css("display","none");
+      $('.isdelhi').css("display", "none");
+  });
+  $('#beng').click(function() {
+
+    	$('.isbengaluru').css("display","block");
+   	$('.ismumbai').css("display","none");
+   		$('.iskolkata').css("display","none");
+      $('.isdelhi').css("display", "none");
+  });
+  $('#kol').click(function() {
+
+    	$('.iskolkata').css("display","block");
+   	$('.isbengaluru').css("display","none");
+   		$('.ismumbai').css("display","none");
+      $('.isdelhi').css("display", "none");
+  });
+  $('#del').click(function() {
+
+    	$('.isdelhi').css("display","block");
+   	$('.isbengaluru').css("display","none");
+   		$('.iskolkata').css("display","none");
+      $('.ismumbai').css("display", "none");
+  });
+ $("#mobi").click();
+ $('#beng').click();
+ $('#exp').click();
+});
